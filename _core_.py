c6e62eb3bdd0760e23aa2eb9550ecdc78b862a21
Arjun Sinha <radiantdragon555888@gmail.com>
2026-01-22 19:53:43 +0530

    Core version 0.1 Added - Only basic window object functionality implemented

---
+++ _core_.py
@@ -0,0 +1,61 @@
+'''Started on 22/01/2026 by Arqatron'''
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+import pymysql
+
+root=tk.Tk()
+root.withdraw()
+
+class window:
+    
+    def create(self,name,height=400,width=500):
+        self.menus={}
+        
+        self.name=tk.Toplevel(root)
+        self.name.geometry(f'{width}x{height}')
+        self.name.resizable(False,False)
+        self.name.title(str(name))
+       
+
+    def menubar_init(self):
+        self.menubar=Menu(self.name)
+        self.name.config(menu=self.menubar)
+        
+        
+    def menu_add(self, header, *options):
+        self.menus[header] = options
+        
+        self.menu = Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label=header, menu=self.menu)
+        for i in options:
+            
+            for j in i:
+               self.menu.add_command(label=j[0],command=j[1])
+
+        
+    def del_option(self, name):
+          for header in self.menus.keys():
+              for opts in self.menus[header]:
+                        temp=[]
+                        for rec in opts:
+                            if rec[0].lower()!=name.lower():
+                               temp.append(rec)
+                        self.menubar.delete(header)
+                        self.menus[header]=temp
+                        self.menu_add(header,tuple(temp))
+                       
+    def del_header(self,name):
+        if name in self.menus.keys():
+            self.menubar.delete(name)
+        
+     
+main=window()
+main.create('Test')
+main.menubar_init()
+main.menu_add('File',(('Test',''),('Test1','')))
+main.del_option('Test1')
+
+
+
+root.mainloop()
